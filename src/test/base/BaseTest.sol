f4b1b701fce2d2909d64c39138b8546affaa9988
Nejc Drobnic <ethoarx@gmail.com>
2022-01-23 19:42:57 +0100

    refactor: list contract source

--- src/test/base/BaseTest.sol
+++ src/test/base/BaseTest.sol
@@ -1,4 +1,7 @@
+// SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.6;
+
+// Source: https://github.com/ZeframLou/playpen/blob/2eb6ff4722e8d8d6ef3a40629e985ce9eb99487e/src/test/base/BaseTest.sol
 
 import {DSTest} from "ds-test/test.sol";
 
--- src/test/utils/IVM.sol
+++ src/test/utils/IVM.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.6;
 
+// Source: https://github.com/ZeframLou/playpen/blob/2eb6ff4722e8d8d6ef3a40629e985ce9eb99487e/src/test/utils/VM.sol
+
 // used at address 0x7109709ECfa91a80626fF3989D68f67F5b1DD12D
 interface IVM {
 	// Set block.timestamp (newTimestamp)
