52a4d1e62f988216de76342549c7055c4e3b8cae
Nejc Drobnic <ethoarx@gmail.com>
2022-01-23 17:57:04 +0100

    refactor: dont test for existence when fetching

--- constants.hardhat.ts
+++ constants.hardhat.ts
@@ -1,2 +1,4 @@
 export const WETHAddress = '0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000';
 export const BOBA_MAINNET_READONLY_RPC = 'https://lightning-replica.boba.network';
+
+export const zeroAddress = '0x0000000000000000000000000000000000000000';
