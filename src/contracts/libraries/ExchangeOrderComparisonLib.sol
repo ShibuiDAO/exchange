d6dec105b7d15602e1a684d7fc9fcd39d7e64a78
Nejc Drobnic <ethoarx@gmail.com>
2022-01-25 16:39:10 +0100

    refactor: compare directly instead of hashing

--- src/contracts/libraries/ExchangeOrderComparisonLib.sol
+++ src/contracts/libraries/ExchangeOrderComparisonLib.sol
@@ -4,19 +4,19 @@ pragma abicoder v2;
 import {IExchange} from "../interfaces/IExchange.sol";
 
 library ExchangeOrderComparisonLib {
-	/// @notice Hashes and compares 2 SellOrder instances to determine if they have the same parameters.
-	/// @param _left SellOrder instance to be hashed and compared on the left side of the operator.
-	/// @param _right SellOrder instance to be hashed and compared on the right side of the operator.
+	/// @notice Compares 2 SellOrder instances to determine if they have the same parameters.
+	/// @param _left SellOrder instance to be compared on the left side of the operator.
+	/// @param _right SellOrder instance to be compared on the right side of the operator.
 	/// @return A boolean value indication if the 2 SellOrder instances match.
 	function compareSellOrders(IExchange.SellOrder memory _left, IExchange.SellOrder memory _right) internal pure returns (bool) {
-		return keccak256(abi.encode(_left)) == keccak256(abi.encode(_right));
+		return (_left.expiration == _right.expiration) && (_left.price == _right.price);
 	}
 
-	/// @notice Hashes and compares 2 BuyOrder instances to determine if they have the same parameters.
-	/// @param _left BuyOrder instance to be hashed and compared on the left side of the operator.
-	/// @param _right BuyOrder instance to be hashed and compared on the right side of the operator.
+	/// @notice Compares 2 BuyOrder instances to determine if they have the same parameters.
+	/// @param _left BuyOrder instance to compared on the left side of the operator.
+	/// @param _right BuyOrder instance to be compared on the right side of the operator.
 	/// @return A boolean value indication if the 2 BuyOrder instances match.
 	function compareBuyOrders(IExchange.BuyOrder memory _left, IExchange.BuyOrder memory _right) internal pure returns (bool) {
-		return keccak256(abi.encode(_left)) == keccak256(abi.encode(_right));
+		return (_left.owner == _right.owner) && (_left.expiration == _right.expiration) && (_left.offer == _right.offer);
 	}
 }
